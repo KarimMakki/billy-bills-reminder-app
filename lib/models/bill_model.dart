a88436969bc4e93f5d219ed68fb66722f2e78ac4
KarimMakki <kmakki48@gmail.com>
2024-11-12 09:14:49 +0800

    added the bill amount property in the billModel class, added BillDuration enum which is responsible on determining the duration of the bill whether it's yearly weekly etc.., added statusColor variable which helps on determining the statusColor of the billstatus, added getDuration variable to get the text on each billDuration, added getDueDate variable to get the due date in a well formatted way

--- lib/models/bill_model.dart
+++ lib/models/bill_model.dart
@@ -1,20 +1,27 @@
+import 'package:billy_bills_reminder_app/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 // ignore: constant_identifier_names
 enum BillStatus { Upcoming, Overdue, Paid }
 
+// ignore: constant_identifier_names
+enum BillDuration { Daily, Weekly, Monthly, Yearly }
+
 class BillModel {
   final String title;
   final String category;
+  final double amount;
   final IconData icon;
   final DateTime dueDate;
   final DateTime reminderTime;
-  final String duration;
+  final BillDuration duration;
   final String? additionalNote;
   final BillStatus billStatus;
   BillModel(
       {required this.title,
       required this.category,
+      required this.amount,
       required this.icon,
       required this.dueDate,
       required this.reminderTime,
@@ -32,4 +39,30 @@ class BillModel {
         return 'Paid';
     }
   }
+
+  Color get statusColor {
+    switch (billStatus) {
+      case BillStatus.Upcoming:
+        return upcomingColor;
+      case BillStatus.Overdue:
+        return overdueColor;
+      case BillStatus.Paid:
+        return paidColor;
+    }
+  }
+
+  String get getDuration {
+    switch (duration) {
+      case BillDuration.Daily:
+        return 'Daily';
+      case BillDuration.Weekly:
+        return "Weekly";
+      case BillDuration.Monthly:
+        return "Monthly";
+      case BillDuration.Yearly:
+        return "Yearly";
+    }
+  }
+
+  String get getDueDate => DateFormat("MMM dd").format(dueDate);
 }
